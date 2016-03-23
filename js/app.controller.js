8206a016c4d898f53c6ce80683ca7d8f94ab7070
Russell Pate <russ@russpate.com>
2016-03-22 23:27:38 -0400

    its responsive now and fixing contrast issues with dynamic color

--- js/app.controller.js
+++ js/app.controller.js
@@ -9,18 +9,16 @@ angular
        $route.reload();
       }
       angular.element(document).find('body').css('background-color', "#" + $scope.hex);
-
     });
-
-
-
   })
-  .controller('SchemeController', function($scope, SchemeService){
+  .controller('SchemeController', function($scope, SchemeService, $route){
     SchemeService.getScheme().then(function(data){
       console.log("data", data);
       $scope.schemeName = data.data.schemes[0].tags[0].name;
       $scope.schemeHex = data.data.schemes[0].colors;
-      angular.element(document).find('article').css('background-color', $scope.hex);
-    })
-
+      $scope.reloadRoute = function() {
+       $route.reload();
+      }
+      angular.element(document).find('body').css('background-color', "white");
+    });
   })
